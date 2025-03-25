97428afeabea2234af7408b91856429a5a530066
Vishoo <vverma971@gmail.com>
2025-03-25 22:17:10 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { io } from 'socket.io-client';
+import { useEffect, useMemo, useState } from 'react';
+
+const App = () => {
+  const socket = useMemo(() => io('http://localhost:3000'), []);
+
+  const [socketId, setSocketId] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [room, setRoom] = useState('');
+
+
+  const handleSubmit= (e)=>{
+    e.preventDefault();
+    const message = e.target[0].value;
+    console.log(message);
+    const reciever = e.target[1].value;
+
+    //socket.emit would send the even called message to the server 
+    socket.emit('message', {message, reciever});
+    e.target[0].value = '';
+    setRoom("");
+  }
+
+  const joinRoom = (e)=>{
+    e.preventDefault();
+    
+    
+    socket.emit('join-room', room);
+    e.target[0].value = '';
+  }
+
+
+
+  useEffect(()=>{
+    socket.on('connect', ()=>{
+      setSocketId(socket.id); 
+      console.log(`Connected with server's socket id ${socketId}`);
+    })
+
+    socket.on('fwd-message', (msg)=>{
+      setMessages([...messages, msg]);
+    })
+  })
+
+  return (
+    <div>
+        <h1>Welcome to our chat application</h1>
+        <h1>{socketId}</h1>
+        <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Enter your message" />
+        <input type="text" placeholder="Reciever's id" />
+        <button type="submit">Send</button>
+
+        
+        </form>
+
+        <form onSubmit={joinRoom} onChange={(e)=>setRoom(e.target.value)}>
+        <input type="text" placeholder="Room's id" />
+        <button type="submit">Join Room</button>
+        </form>
+
+        {/* messages will be displayed here */}
+        <ul>
+          {messages.map((msg, index)=>{
+            
+            return <li key={index}>{msg}</li>
+
+          })}
+        </ul>
+        
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+// 1. setup the express server
+const cors= require('cors');
+app.use(cors());
+
+const http = require('http').createServer(app);
+//2. this creates a server that will be used to establish 
+//live connection for our socket.io server
+
+// const io = require('socket.io')(http);
+const socket = require('socket.io');
+// 3. socket import
+
+const io = new socket.Server(http, {
+    cors:{
+        origin: '*',
+        methods: ['GET', 'POST']
+
+    }
+});
+// 4. we used the http server to create a socket.io server
+
+io.on("connection", (socket)=>{
+    console.log(`a user connect with socket id ${socket.id}`);
+
+    socket.on('message', (data)=>{
+        console.log(data.message);
+        // io.emit('fwd-message', data.message);
+        io.to(data.reciever).emit('fwd-message', data.message);
+    });
+
+
+    socket.on('join-room', (room)=>{
+        socket.join(room);
+        console.log(`User with socket id ${socket.id} joined room ${room}`);
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+
+
+http.listen(3000, ()=>{
+    console.log('Server is running on port 3000');
+})
